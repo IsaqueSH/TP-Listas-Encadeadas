fc0138ac3cc1575cb9436358dd7f4310f7563b5d
GheRodrigues <129014348+GheRodrigues@users.noreply.github.com>
2026-02-01 15:06:18 -0300

    Update structs_trabalho_final.h

--- structs_trabalho_final.h
+++ structs_trabalho_final.h
@@ -3,5 +3,35 @@
 
 #include <stdlib.h>
 
+typedef struct list {
+	struct cliente *head; 
+	int size;
+}List;
+
+typedef struct cliente {
+    char cpf[15];
+    char *nome;
+    char *email;
+    char *telefone;
+    struct data nascimento; 
+    int qt_produtos;
+    struct cliente *next;
+    struct produto *lista_produtos; 
+}Cliente;
+
+typedef struct data {
+    int dia;
+    int mes;
+    int ano;
+}Data;
+
+typedef struct produto {
+    char *codigo;
+    char *nome;
+    float preco;
+    int quantidade;
+    struct produto *next;
+}Produto;
 
 #endif
+
