1b68de14fc1f0025b0a27abeb3f3c4ba66d5f933
Geovanna Rodrigues de Brito <129014348+GheRodrigues@users.noreply.github.com>
2026-02-01 21:54:52 -0300

    Update funcoes.c

--- funcoes.c
+++ funcoes.c
@@ -4,7 +4,173 @@
     Método que roda a main principal do Trabalho Final.
 */
 void main_trabalho_final() {
+    int opcao = -1;
+    List * list = NULL;
+    Cliente *clientes = NULL;
 
-    
+    list = create_list();
+
+    menu_principal();
 }
 
+
+/*
+    Método que aloca o espaço na memória e retorna a lista criada.
+*/
+List *create_list() {
+    List * list = (List *) malloc(sizeof(List));
+    list->head = NULL;
+    list->size = 0;
+
+    return list;
+}
+
+
+/*
+    Método que mostra o menu principal do Software e 
+    permite ao usuário escolher uma opção.
+*/
+void menu_principal() {
+    int opcao = 0;
+    
+    printf("----------------- MENU PRINCIPAL ----------------------\n");
+    printf("*            (1) Gerenciamento de Clientes            *\n");
+    printf("*            (2) Gerenciamento de Produtos            *\n");
+    printf("*            (3) Modo Compra                          *\n");
+    printf("*            (4) Sair do Programa                     *\n");
+    printf("-------------------------------------------------------\n");
+    printf("Digite a opcao desejada --> ");
+    scanf("%d", &opcao);	
+
+     do {  
+        switch(opcao) {
+            case 1:
+                menu_cliente();
+                break;
+
+            case 2:
+                menu_produto();
+                break;
+
+            case 3:
+                //função modo compra
+                break;
+            case 4:
+                menu_encerra();
+                break;
+            default:
+                printf("Opcao Invalida...\n");
+                sleep(1);
+                break;
+        }
+
+    } while(opcao != 4);
+    return;
+}
+
+/*
+    Método que mostra o menu cliente do Software e 
+    permite ao usuário escolher uma opção.
+*/
+void menu_cliente() {
+    system("cls");
+    int opcao = 0;
+
+    printf("----------------- MENU CLIENTE ------------------\n");
+    printf("*            (1) Cadastrar Cliente              *\n");
+    printf("*            (2) Listar Cliente                 *\n");
+    printf("*            (3) Visualizar Cliente             *\n");
+    printf("*            (4) Editar Cliente                 *\n");
+    printf("*            (5) Excluir Cliente                *\n");
+    printf("*            (6) Menu Principal                 *\n");
+    printf("-------------------------------------------------\n");
+    printf("Digite a opcao desejada --> ");
+    scanf("%d", &opcao);	
+
+    do { 
+        switch(opcao) {
+            case 1:
+                
+                break;
+
+            case 2:
+                
+                break;
+
+            case 3:
+            
+                break;
+
+            case 4:
+            
+                break;
+
+            case 5:
+                
+                break;
+
+            case 6:
+                menu_principal();
+                break;
+            
+            default:
+                printf("Opcao Invalida...\n");
+                sleep(1);
+                break;
+        }
+    } while(opcao != 6);
+}
+
+void menu_produto() {
+    system("cls");
+    int opcao = 0;
+    
+    printf("----------------- MENU PRODUTO ------------------\n");
+    printf("*            (1) Cadastrar Produto              *\n");
+    printf("*            (2) Listar Produto                 *\n");
+    printf("*            (3) Visualizar Produto             *\n");
+    printf("*            (4) Editar Produto                 *\n");
+    printf("*            (5) Excluir Produto                *\n");
+    printf("*            (6) Menu Principal                 *\n");
+    printf("-------------------------------------------------\n");
+    printf("Digite a opcao desejada --> ");
+    scanf("%d", &opcao);	
+
+    do { 
+        switch(opcao) {
+            case 1:
+                
+                break;
+
+            case 2:
+                
+                break;
+
+            case 3:
+            
+                break;
+
+            case 4:
+            
+                break;
+
+            case 5:
+                
+                break;
+
+            case 6:
+                menu_principal();
+                break;
+            
+            default:
+                printf("Opcao Invalida...\n");
+                sleep(1);
+                break;
+        }
+    } while(opcao != 6);
+}
+
+void menu_encerra() {
+    printf("\n\n\n\tAte uma proxima vez....\n");
+    sleep(1.5);
+}
