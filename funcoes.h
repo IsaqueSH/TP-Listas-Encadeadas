ddc45b0591e81138483522bd05d4d239e4a7af88
Geovanna Rodrigues de Brito <129014348+GheRodrigues@users.noreply.github.com>
2026-02-01 21:54:12 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -3,8 +3,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
 #include "structs_trabalho_final.h"
 
 void main_trabalho_final();
+List *create_list();
+void menu_principal();
+void menu_cliente();
+void menu_produto();
+void menu_encerra();
 
 #endif
